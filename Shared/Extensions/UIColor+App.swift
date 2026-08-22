079041fd839a115bcf3762d55fe01c6a360f90b8
Café MegaByte <67206705+cafe-megabyte@users.noreply.github.com>
2026-08-23 01:53:43 +0200

    AppTint

--- Shared/Extensions/UIColor+App.swift
+++ Shared/Extensions/UIColor+App.swift
@@ -1,5 +1,9 @@
 import UIKit
 
 extension UIColor {
-    static let appTint = UIColor.systemRed
+    static let appTint = UIColor(    red: 204 / 255.0,
+                                     green: 33 / 255.0,
+                                     blue: 49 / 255.0,
+                                     alpha: 1.0
+    )
 }
