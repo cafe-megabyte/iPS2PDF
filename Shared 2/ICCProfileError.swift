f6fdab1b036666c65dd176c3c95ed2109287ed1f
Café MegaByte <67206705+cafe-megabyte@users.noreply.github.com>
2026-08-22 16:16:27 +0200

    Remove unused files

--- Shared 2/ICCProfileError.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum ICCProfileError: LocalizedError {
-    case invalidHeader
-
-    var errorDescription: String? {
-        String(localized: "The file does not contain a valid ICC profile header.")
-    }
-}
